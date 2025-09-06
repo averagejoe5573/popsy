8ca043efdcdba1f04b8ea14c57827162164c373a
averagejoe553 <averagejoe5573@gmail.com>
2025-09-06 15:30:53 +0100

    feat: update utils

--- src/system/utils.ts
+++ src/system/utils.ts
@@ -1,20 +1,21 @@
 import { MIN_NODE_VERSION } from "../common/constants";
 
-export function checkNodeVersion(minMajor: number = MIN_NODE_VERSION): void {
-    const nodeVersion = process.versions.node;
+export function checkNodeVersion(minNodeVersion: number = MIN_NODE_VERSION): void {
+    const _nodeVersion = process.versions.node;
+    const nodeVersion = parseInt(_nodeVersion)
 
-    if (typeof nodeVersion !== "string") {
+    if (!nodeVersion) {
         throw new Error("Unable to detect Node.js version");
     }
 
-    if (isNaN(parseInt(nodeVersion))) {
+    if (isNaN(nodeVersion)) {
         throw new Error(`Invalid Node.js version format: ${nodeVersion}`);
     }
 
-    if (parseInt(nodeVersion) < 18) {
-        console.error(`Node.js version ${minMajor} is not supported. Please use Node 18 or newer.`);
+    if (nodeVersion < minNodeVersion) {
+        console.error(
+            `Node.js version ${nodeVersion} is not supported. Please use Node ${minNodeVersion} or newer.`
+        );
         process.exit(1);
     }
-
-    console.log(parseInt(nodeVersion))
 }
